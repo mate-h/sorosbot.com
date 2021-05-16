98da93fea81ecc3e84c43a9037786608dc8df18d
Máté Homolya <mate.homolya@gmail.com>
2021-05-16 16:47:07 -0600

    Api authentication

--- api/src/index.ts
+++ api/src/index.ts
@@ -1,6 +1,12 @@
 // Require the framework and instantiate it
 import fastify from 'fastify';
+import cookie from 'cookie';
+import * as admin from 'firebase-admin';
+
 const server = fastify({ logger: true });
+
+let app = admin.initializeApp();
+const auth = app.auth();
 
 // Declare a route
 server.get('/', async (request, reply) => {
@@ -9,9 +15,43 @@ server.get('/', async (request, reply) => {
 server.get('/auth', async (request, reply) => {
   // validate request headers
   // bearer token in authorization or session cookie
-  reply.code(200).send({
-    headers: request.headers
-  });
+  const cookies = cookie.parse(request.headers.cookie || '');
+  const authHeader = request.headers.authorization || '';
+  let idToken, user;
+  if (authHeader.toLowerCase().includes('bearer')) {
+    idToken = authHeader.split(' ')[1];
+  }
+  let authorized = false;
+  try {
+    if (idToken) {
+      user = await auth.verifyIdToken(idToken);
+      authorized = user !== undefined;
+    }
+    else {
+      const session = cookies.session;
+      if (session) {
+        user = await auth.verifySessionCookie(session);
+        authorized = user !== undefined;
+      }
+    }
+  } catch(e) {
+    reply.code(401).send(e);
+  }
+
+  if (authorized) {
+    reply.code(200).send({
+      user,
+      cookies,
+      headers: request.headers
+    });
+  }
+  else {
+    reply.code(401).send({
+      user,
+      cookies,
+      headers: request.headers
+    });
+  }
 })
 
 // Run the server!
