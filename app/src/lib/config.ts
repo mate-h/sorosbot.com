6e999abcaca83fcc844a04efebf7ef48563c5d71
Máté Homolya <mate.homolya@gmail.com>
2021-05-18 12:28:53 -0600

    Update config

--- app/src/lib/config.ts
+++ app/src/lib/config.ts
@@ -1,3 +1,3 @@
-export const apiHost = "http://localhost:5000"; //process.env["API_HOST"];
+export const apiHost = "https://api.sorosbot.com"; //process.env["API_HOST"];
 export const codeHost = "https://code.sorosbot.com"; //process.env["CODE_HOST"];
 export const cookieDomain = `.sorosbot.com`;
