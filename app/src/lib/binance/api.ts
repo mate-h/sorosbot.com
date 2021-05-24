2e0ea31e181faad92e4a6e71e2fe36d3fafef74d
Máté Homolya <mate.homolya@gmail.com>
2021-05-24 16:19:01 -0600

    Fix binance api SSR error

--- app/src/lib/binance/api.ts
+++ app/src/lib/binance/api.ts
@@ -1,7 +1,4 @@
 import Binance from 'binance-api-node'
-
-export const client = Binance();
-
-export function getCandles() {
-  return fetch(`https://api.binance.com/api/v3/klines?symbol=BTCUSDT&interval=5m`).then(r => r.json());
-}
+export let client;
+if (typeof Binance === 'function')
+  client = Binance();
